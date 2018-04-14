c91f87e744a051f477496f586e9c6426744538da
Joan Albert Segura <jalbertsr@protonmail.ch>
2018-04-14 22:31:43 +0200

    add photo profile

--- adidas/src/Client.js
+++ adidas/src/Client.js
@@ -1,5 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Row, Col } from "antd";
+import { Card } from "antd";
+import "./Client.css";
 
-const Client = () => <div>client!</div>;
+const { Meta } = Card;
+
+class Client extends Component {
+  state = {
+    imageUrl:
+      "http://www.adidas.es/static/on/demandware.static/-/Sites-adidas-ES-Library/default/dw439383d6/help/ico-company.png",
+    data: []
+  };
+
+  componentDidMount() {
+    //fetch data
+  }
+
+  render() {
+    const clientId = this.props.match.params.id;
+    const { imageUrl } = this.state;
+    return (
+      <React.Fragment>
+        <div className="wrapper">
+          <Row>
+            <Col span={6}>
+              <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="profile" src={imageUrl} />}
+              >
+                <Meta title="ClientId:" description={clientId} />
+              </Card>
+            </Col>
+            <Col span={10} />
+            <Col span={6} />
+          </Row>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Client;
