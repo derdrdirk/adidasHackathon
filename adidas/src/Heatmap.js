d57ed30d03b1fb1a19eac51d175e44726696a69b
knowledge91 <dirkhornung91@gmail.com>
2018-04-15 04:53:20 +0200

    timeseries

--- adidas/src/Heatmap.js
+++ adidas/src/Heatmap.js
@@ -3,7 +3,29 @@ import { FlexibleXYPlot, HeatmapSeries } from 'react-vis';
 
 class Heatmap extends Component {
   render() {
-    const myData = [
+    const { faces, selectedEmtion } = this.props;
+
+    const zeroVector = [0, 0, 0, 0, 0, 0, 0, 0 ,0, 0]
+    const heatMatrix = [];
+    for(var i=0; i<10; i++) {
+      heatMatrix[i] = zeroVector;
+    }
+
+    /* for (let i=0; i<100; i++) {
+     *   const x = Math.round(Math.random()*10)-1;
+     *   const y = Math.round(Math.random()*10)-1;
+
+       /* heatMatrix[x][y]; */
+
+
+    /* const data = [];
+     * for (let i=0; i<9; i++) {
+     *   for (let j=0; j<9; j++) {
+     *     data.push({x: i+1, y: j+1, color: heatMatrix[i][j] })
+     *   }
+     * } */
+
+    const data = [
       {x: 1, y: 0, color: 10},
       {x: 1, y: 5, color: 10},
       {x: 1, y: 10, color: 6},
@@ -23,7 +45,7 @@ class Heatmap extends Component {
         height={250}>
         <HeatmapSeries
           className="heatmap-series-example"
-          data={myData}/>
+          data={data}/>
       </FlexibleXYPlot>
     );
   }
